847df2ccadf41b4419a2617ac168e0fd0c315dc6
yukiy4n4g1 <40228235+yukiy4n4g1@users.noreply.github.com>
2024-05-12 23:38:12 +0900

    add eza

--- home/tui/default.nix
+++ home/tui/default.nix
@@ -25,5 +25,11 @@
     userName = "yukiy4n4g1";
     userEmail = "40228235+yukiy4n4g1@users.noreply.github.com";
   };
+
+  programs.eza = {
+    enable = true;
+    git = true;
+    icons = true;
+  };
 }
 
