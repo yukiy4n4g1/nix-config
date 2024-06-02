ac31f9f477adc75533ecc5c3c4c7d8a7fad0fc85
yukiy4n4g1 <40228235+yukiy4n4g1@users.noreply.github.com>
2024-06-02 11:26:25 +0900

    services.xserver→services.xserver.xkb

--- hosts/dell-inspiron/configuration.nix
+++ hosts/dell-inspiron/configuration.nix
@@ -55,9 +55,9 @@
   security.pam.services.swaylock = {};
 
   # Configure keymap in X11
-  services.xserver = {
+  services.xserver.xkb = {
     layout = "us";
-    xkbVariant = "";
+    variant = "";
   };
 
   # Enable CUPS to print documents.
