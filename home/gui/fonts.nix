22676b59f3cd306f51c1e558c4af91d502ef52d7
yukiy4n4g1 <40228235+yukiy4n4g1@users.noreply.github.com>
2024-12-12 21:54:28 +0900

    add font setting

--- home/gui/fonts.nix
+++ home/gui/fonts.nix
@@ -2,8 +2,20 @@
 
 {
   home.packages = with pkgs; [
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
+    noto-fonts-color-emoji
     plemoljp-nf
   ];
 
-  fonts.fontconfig.enable = true;
+  fonts.fontconfig = {
+    enable = true;
+
+    defaultFonts = {
+      serif = [ "NotoSerifCJK-VF" ];
+      sansSerif = [ "NotoSansCJK-VF" ];
+      monospace = [ "NotoSansMonoCJK-VF" ];
+      emoji = [ "NotoColorEmoji" ];
+    };
+  };
 }
