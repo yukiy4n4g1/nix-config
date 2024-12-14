7b2c55645d7c0e54974d0fe95b81f028fe7a9287
yukiy4n4g1 <40228235+yukiy4n4g1@users.noreply.github.com>
2024-12-14 23:15:16 +0900

    swayに壁紙の設定を追加

--- home/gui/sway/default.nix
+++ home/gui/sway/default.nix
@@ -1,9 +1,23 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 {
+  home.packages = with pkgs; [
+    swaybg
+    cosmic-wallpapers
+  ];
+
   wayland.windowManager.sway = {
     enable = true;
     wrapperFeatures.gtk = true;
+    checkConfig = false;
+
+    config = {
+      output = {
+        "*" = {
+          bg = "${config.xdg.stateHome}/home-manager/gcroots/current-home/home-path/share/backgrounds/cosmic/webb-inspired-wallpaper-system76.jpg fill";
+        };
+      };
+    };
 
     extraConfig = builtins.readFile ./config;
   };
