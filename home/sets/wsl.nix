fcafd0a3efb9dfce305974fb04f31a88ac7ed780
yukiy4n4g1 <40228235+yukiy4n4g1@users.noreply.github.com>
2025-01-04 01:57:37 +0000

    add neovim setting

--- home/sets/wsl.nix
+++ home/sets/wsl.nix
@@ -5,11 +5,12 @@
   imports = [
     ../home.nix
 
+    ../tui/bash/default.nix
     ../tui/git.nix
     ../tui/lazygit.nix
+    ../tui/neovim/default.nix
     ../tui/packages.nix
     ../tui/starship.nix
     ../tui/wslu.nix
-    ../tui/bash/default.nix
   ];
 }
