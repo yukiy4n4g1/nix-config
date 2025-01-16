d8ab654f0cc2e2e0a58e4b0200ea8547a9e61190
yukiy4n4g1 <40228235+yukiy4n4g1@users.noreply.github.com>
2025-01-16 23:38:56 +0900

    add home-manager setting for MacOS

---
+++ home/sets/darwin.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = builtins.getEnv "USER";
+  home.homeDirectory = builtins.getEnv "HOME";
+  home.stateVersion = "24.11";
+  programs.home-manager.enable = true;
+
+  imports = [
+    ../tui/helix.nix
+  ];
+}
