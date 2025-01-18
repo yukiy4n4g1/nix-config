e02a4b4ab639707a07158b5975adaeed838770c9
yukiy4n4g1 <40228235+yukiy4n4g1@users.noreply.github.com>
2025-01-18 23:13:25 +0900

    gh: enable

--- home/tui/packages.nix
+++ home/tui/packages.nix
@@ -4,13 +4,14 @@
   home.packages = with pkgs; [
     bat
     neofetch
-    gh
     htop
     fd
     ripgrep
   ];
 
   programs.fzf.enable = true;
+
+  programs.gh.enable = true;
 
   programs.yazi = {
     enable = true;
