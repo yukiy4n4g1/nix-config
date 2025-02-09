f155b51981f62088416eac79cd0930a25fa412e6
yukiy4n4g1 <40228235+yukiy4n4g1@users.noreply.github.com>
2025-02-09 23:33:49 +0900

    kanshi: ディスプレイの設定を更新

--- home/gui/kanshi.nix
+++ home/gui/kanshi.nix
@@ -1,5 +1,3 @@
-{ pkgs, ... }:
-
 {
   services.kanshi = {
     enable = true;
@@ -20,13 +18,13 @@
           {
             criteria = "Dell Inc. DELL U2720QM 15FXX83";
             position = "0,0";
-            scale = 2.0;
+            scale = 1.8;
             mode = "3840x2160@59.997Hz";
           }
           {
             criteria = "eDP-1";
-            position = "443,1080";
-            scale = 1.3;
+            position = "0,1200";
+            scale = 1.0;
             status = "enable";
           }
         ];
