352c043e750bdbbf0630af6926c3bae94eecfe72
yukiy4n4g1 <40228235+yukiy4n4g1@users.noreply.github.com>
2025-02-11 01:00:07 +0900

    add extraSpecialArgs

--- flake.nix
+++ flake.nix
@@ -27,7 +27,8 @@
           home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-
+            home-manager.extraSpecialArgs = { pkgs-unstable = nixpkgs-unstable; };
+            
             home-manager.users.yukiy4n4g1 = (import ./home/sets/linux-wayland.nix inputs);
           }
         ];
