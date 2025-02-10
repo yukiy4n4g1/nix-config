162a4424d06534c0b7e72bbf75e317d884026522
yukiy4n4g1 <40228235+yukiy4n4g1@users.noreply.github.com>
2025-02-10 23:04:17 +0900

    sway: workspaceのoutput設定

--- home/gui/sway/default.nix
+++ home/gui/sway/default.nix
@@ -21,6 +21,19 @@
           bg = "${config.xdg.stateHome}/home-manager/gcroots/current-home/home-path/share/backgrounds/cosmic/webb-inspired-wallpaper-system76.jpg fill";
         };
       };
+
+      workspaceOutputAssign = [
+        { workspace = "1"; output = "eDP-1"; }
+        { workspace = "2"; output = "DP-1"; }
+        { workspace = "3"; output = "DP-1"; }
+        { workspace = "4"; output = "DP-1"; }
+        { workspace = "5"; output = "DP-1"; }
+        { workspace = "6"; output = "DP-1"; }
+        { workspace = "7"; output = "DP-1"; }
+        { workspace = "8"; output = "DP-1"; }
+        { workspace = "9"; output = "DP-1"; }
+        { workspace = "10"; output = "DP-1"; }
+      ];
     };
 
     extraConfig = builtins.readFile ./config;
