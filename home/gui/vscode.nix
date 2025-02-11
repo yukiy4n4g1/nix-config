cb3f7800f2db19e2de540ef30e57e71e4f01734b
yukiy4n4g1 <40228235+yukiy4n4g1@users.noreply.github.com>
2025-02-12 01:06:36 +0900

    vscode: 拡張機能の追加

--- home/gui/vscode.nix
+++ home/gui/vscode.nix
@@ -1,12 +1,29 @@
-{ pkgs, ... }:
+{ pkgs-unstable, ... }:
 
 {
   programs.vscode = {
+    package = pkgs-unstable.vscode.fhs;
     enable = true;
-    extensions = with pkgs.vscode-extensions; [
+    enableUpdateCheck = false;
+    extensions = with pkgs-unstable.vscode-extensions; [
       enkia.tokyo-night
       vscodevim.vim
       jnoortheen.nix-ide
+      vscode-extensions.github.copilot
+      vscode-extensions.github.copilot-chat
+    ] ++ pkgs-unstable.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "vscode-language-pack-ja";
+        publisher = "ms-ceintl";
+        version = "1.97.2025020509";
+        sha256 = "0xz5kkq0scmxfvn27lpgb0lz7dzp14fhr733sd5ky158wlbminx4";
+      }
+      {
+        name = "symbols";
+        publisher = "miguelsolorio";
+        version = "0.0.22";
+        sha256 = "0k644ma94k6ygk6ys8rgmrz2q0ixjkvkik4393p44fking7k55id";
+      }
     ];
   };
 }
