957ae624a0197ff2bb8fe35f669640b06cda295f
yukiy4n4g1 <40228235+yukiy4n4g1@users.noreply.github.com>
2025-11-10 23:28:55 +0900

    update kanshi setting

--- home/gui/kanshi.nix
+++ home/gui/kanshi.nix
@@ -2,33 +2,39 @@
   services.kanshi = {
     enable = true;
 
-    profiles = { 
-      default = {
-        outputs = [
-          {
-            criteria = "eDP-1";
-            position = "0,0";
-            scale = 1.0;
-            status = "enable";
-          }
-        ];
-      };
-      home1 = {
-        outputs = [
-          {
-            criteria = "Dell Inc. DELL U2720QM 15FXX83";
-            position = "0,0";
-            scale = 1.8;
-            mode = "3840x2160@59.997Hz";
-          }
-          {
-            criteria = "eDP-1";
-            position = "0,1200";
-            scale = 1.0;
-            status = "enable";
-          }
-        ];
-      };
-    };
+    settings = [
+      {
+        profile = {
+          name = "default";
+          outputs = [
+            {
+              criteria = "eDP-1";
+              position = "0,0";
+              scale = 1.0;
+              status = "enable";
+            }
+          ];
+        };
+      }
+      {
+        profile = {
+          name = "home1";
+          outputs = [
+            {
+              criteria = "Dell Inc. DELL U2720QM 15FXX83";
+              position = "0,0";
+              scale = 1.8;
+              mode = "3840x2160@59.997Hz";
+            }
+            {
+              criteria = "eDP-1";
+              position = "0,1200";
+              scale = 1.0;
+              status = "enable";
+            }
+          ];
+        };
+      }
+    ];
   };
 }
