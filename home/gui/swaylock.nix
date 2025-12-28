9d5ad8ff22029e95026736bc4271e907f1cab668
yukiy4n4g1 <40228235+yukiy4n4g1@users.noreply.github.com>
2025-12-29 00:32:03 +0900

    swaylock: 壁紙を変更

--- home/gui/swaylock.nix
+++ home/gui/swaylock.nix
@@ -1,10 +1,14 @@
 { pkgs, config, ... }:
 
 {
+  home.packages = with pkgs; [
+    cosmic-wallpapers
+  ];
+
   programs.swaylock = {
     enable = true;
     settings = {
-      image = "${config.xdg.stateHome}/home-manager/gcroots/current-home/home-path/share/backgrounds/cosmic/otherworldly_earth_nasa_ISS064-E-29444.jpg";
+      image = "${config.xdg.stateHome}/home-manager/gcroots/current-home/home-path/share/backgrounds/cosmic/round_moons_nasa.jpg";
     };
   };
 }
