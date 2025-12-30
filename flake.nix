f4c64116f6e8237276c4b4ca7d8ca950057c4dea
yukiy4n4g1 <40228235+yukiy4n4g1@users.noreply.github.com>
2025-12-31 00:04:27 +0900

    home-manager backup
    home-managerですでにあるファイルを作成しようとするときに既存のファイルをbackup拡張子をつけてバックアップする

--- flake.nix
+++ flake.nix
@@ -28,6 +28,7 @@
           home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "backup";
             home-manager.extraSpecialArgs = {
               inherit inputs;
             };
