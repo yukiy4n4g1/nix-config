90f62ba890ce56653d74611f3e09c741202ba686
yukiy4n4g1 <40228235+yukiy4n4g1@users.noreply.github.com>
2026-01-11 11:32:13 +0900

    x1: add nix gc setting

--- hosts/x1/configuration.nix
+++ hosts/x1/configuration.nix
@@ -170,6 +170,14 @@
     };
   };
 
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-order-than 14d";
+  };
+
+  nix.settings.auto-optimise-store = true;
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
