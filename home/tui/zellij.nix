29b0ecf4fd02668398693c7fc1181b06103b0f40
yukiy4n4g1 <40228235+yukiy4n4g1@users.noreply.github.com>
2026-01-25 21:35:36 +0900

    zellij: 設定を編集
    uiをシンプルに
    paneのフレームを消す
    起動時のTipsを表示しない

--- home/tui/zellij.nix
+++ home/tui/zellij.nix
@@ -5,6 +5,9 @@
     settings = {
       theme = "tokyo-night";
       default_mode = "locked";
+      simplified_ui = true;
+      show_startup_tips = false;
+      pane_frames = false;
     };
   };
 }
