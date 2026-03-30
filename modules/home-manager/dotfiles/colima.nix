6390c2cd0e483480e4847bb33cfb301a9160adfe
yukiy4n4g1 <40228235+yukiy4n4g1@users.noreply.github.com>
2026-03-31 00:25:58 +0900

    colima: profileではなくtemplateを作成する

--- modules/home-manager/dotfiles/colima.nix
+++ modules/home-manager/dotfiles/colima.nix
@@ -1,29 +1,49 @@
-{ pkgs, ...}:
+{ pkgs, lib, ... }:
 
+let
+  yamlFormat = pkgs.formats.yaml { };
+
+  envCertPath = builtins.getEnv "SSL_CERT_FILE";
+  caCert = if envCertPath != "" then builtins.readFile envCertPath else "";
+
+  colimaConfig = {
+    cpu = 4;
+    memory = 6;
+    disk = 60;
+    arch = "host";
+    runtime = "docker";
+    vmType = "vz";
+    mountType = "virtiofs";
+    rosetta = true;
+    autoActivate = true;
+
+    mounts = [
+      {
+        location = "${builtins.getEnv "HOME"}";
+        writable = true;
+      }
+    ];
+
+    provision = lib.optionals (caCert != "") [
+      {
+        mode = "user";
+        script = ''
+          sudo mkdir -p /usr/local/share/ca-certificates
+          echo '${caCert}' | sudo tee /usr/local/share/ca-certificates/cloudflare.crt > /dev/null
+          sudo update-ca-certificates
+          sudo service docker restart || true
+        '';
+      }
+    ];
+  };
+in
 {
   home.packages = with pkgs; [
     docker
     docker-compose
+    colima
   ];
 
-  home.sessionVariables = {
-    COLIMA_SAVE_CONFIG = "0"; # colimaが設定ファイルを書き換えるのを防止する
-  };
-
-  services.colima = {
-    enable = true;
-    profiles = {
-      default = {
-        settings = {
-          cpu = 4;
-          memory = 6;
-          disk = 60;
-
-          vmType = "vz";
-          mountType = "virtiofs";
-          rosetta = true;
-        };
-      };
-    };
-  };
+  home.file.".colima/_templates/default.yaml".source = 
+    yamlFormat.generate "colima-template.yaml" colimaConfig;
 }
